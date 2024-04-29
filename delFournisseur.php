a8767ecd670b7856219623aed9cafbec4657a758
sergeo12 <168342200+sergeo12@users.noreply.github.com>
2024-04-29 14:30:34 +0100

    Update delFournisseur.php

--- delFournisseur.php
+++ delFournisseur.php
@@ -3,7 +3,7 @@ session_start();
 
 if($_GET){
     if(isset($_GET['num_fournisseur']) && !empty($_GET['num_fournisseur'])){
-        // On inclut la connexion à la base
+        // On infgfyffclut la connexion à la base
         require_once('connexion.php');
 
       $num_fournisseur=$_GET['num_fournisseur'];
